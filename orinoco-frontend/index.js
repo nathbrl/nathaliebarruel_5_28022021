eb5677d399f54a50fbf565ec2a50c92fd1fdc00c
nathbrl <nathalie.barruel@outlook.fr>
2021-03-11 18:20:11 +0100

    Fetch api
    Récupération  des éléments de l'api et insertion dans html

--- orinoco-frontend/index.js
+++ orinoco-frontend/index.js
@@ -1,73 +1,81 @@
-fetch("http://localhost:3000/api/furniture")
-  .then(res => {
-    if(res.ok){
-      res.json().then(data => {
-        img.src = data[0].imageUrl
-      })
-    } else{
-      console.log("error");
-    }
+let data;
+
+// API REQUEST
+const fetchData = async() => {
+  data = await fetch ('http://localhost:3000/api/furniture')
+    .then(res => res.json())
+      console.log(data);
+  data.forEach(element => {
+    console.log(element.name);
+    // IMAGE
+  const image = data[0];
+  const image1 = document.querySelector('.image');
+    image1.innerHtml = image.imageUrl;
+    //NAME
+  const name = data[0];
+  const name1 = document.querySelector('.name');
+    name1.innerHTML = name.name;
+    // VARNISH
+  const varnish = data[0];
+  const varnish1 = document.querySelector('.varnish');
+  varnish1.innerHTML = varnish.varnish;
+    // DESCRIPTION
+  const desc = data[0];
+  const desc1 = document.querySelector('.info');
+    desc1.innerHTML = desc.description;
+    // PRICE
+  const price = data[0];
+  const price1 = document.querySelector('.price');
+    price1.innerHTML = price.price;
   })
-  //.then(res => console.log(res)
-  //.then(data => console.log(data))
+  data.forEach(element => {
+    image = data[1];
+    image2 = document.querySelector('.image1').innerHtml = image.imageUrl;
+    varnish = data[1];
+    varnish2 = document.querySelector('.varnish1').innerHTML = varnish.varnish;
+    desc = data[1];
+    desc2 = document.querySelector('.info1').innerHTML = desc.description;
+    price = data[1];
+    price2 = document.querySelector('.price1').innerHTML = data[1].price;
+    name = data[1];
+    name2 = document.querySelector('.name1').innerHTML = data[1].name;
+  })
+  data.forEach(element => {
+    image = data[2];
+    image3 = document.querySelector('.image2').innerHtml = image.imageUrl;
+    varnish = data[2];
+    varnish3 = document.querySelector('.varnish2').innerHTML = varnish.varnish;
+    desc = data[2];
+    desc3 = document.querySelector('.info2').innerHTML = desc.description;
+    price = data[2];
+    price3 = document.querySelector('.price2').innerHTML = data[2].price;
+    name = data[2];
+    name3 = document.querySelector('.name2').innerHTML = data[2].name;
+  })
+  data.forEach(element => {
+    image = data[3];
+    image4 = document.querySelector('.image3').innerHtml = image.imageUrl;
+    varnish = data[3];
+    varnish4 = document.querySelector('.varnish3').innerHTML = varnish.varnish;
+    desc = data[3];
+    desc4 = document.querySelector('.info3').innerHTML = desc.description;
+    price = data[3];
+    price4 = document.querySelector('.price3').innerHTML = data[3].price;
+    name = data[3];
+    name4 = document.querySelector('.name3').innerHTML = data[3].name;
+  })
+  data.forEach(element => {
+    image = data[4];
+    image5 = document.querySelector('.image4').innerHtml = image.imageUrl;
+    varnish = data[4];
+    varnish5 = document.querySelector('.varnish4').innerHTML = varnish.varnish;
+    desc = data[4];
+    desc5 = document.querySelector('.info4').innerHTML = desc.description;
+    price = data[4];
+    price5 = document.querySelector('.price4').innerHTML = data[4].price;
+    name = data[4];
+    name5 = document.querySelector('.name4').innerHTML = data[4].name;
+  });
+};
 
-const img = document.getElementById("produits");
-
-/*{
-  "varnish":[
-     "Dark Oak",
-     "Light Oak",
-     "Mahogany"
-  ],
-  "_id":"5be9cc611c9d440000c1421e",
-  "name":"Cross Table",
-  "price":59900,
-  "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  "imageUrl":"http://localhost:3000/images/oak_1.jpg"
-},
-
-{
-  "varnish":[
-     "Dark Oak",
-     "Light Oak"
-  ],
-  "_id":"5beaadda1c9d440000a57d98",
-  "name":"Coffee Table",
-  "price":89900,
-  "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  "imageUrl":"http://localhost:3000/images/oak_2.jpg"
-},
-{
-  "varnish":[
-     "Dark Oak",
-     "Teak",
-     "Mahogany"
-  ],
-  "_id":"5beaae361c9d440000a57d99",
-  "name":"Dining Table (extendable)",
-  "price":109900,
-  "imageUrl":"http://localhost:3000/images/oak_3.jpg",
-  "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-},
-{
-  "varnish":[
-     "Light Oak",
-     "Teak"
-  ],
-  "_id":"5beaaf2e1c9d440000a57d9a",
-  "name":"Bench",
-  "price":39900,
-  "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  "imageUrl":"http://localhost:3000/images/oak_4.jpg"
-},
-{
-  "varnish":[
-     "Dark Oak",
-     "Mahogany"
-  ],
-  "_id":"5beab2061c9d440000a57d9b",
-  "name":"Vintage Chair",
-  "price":79900,
-  "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  "imageUrl":"http://localhost:3000/images/oak_5.jpg"
-}*/
+fetchData();
